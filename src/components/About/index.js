2e9a0fc6da0879ebdb97b1863ccaeb264b0491da
sibteali786 <sibteali786@gmail.com>
2022-07-09 01:40:39 +0500

    text animation on about page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -10,6 +10,8 @@ import {
   faNodeJs,
   faReact,
 } from '@fortawesome/free-brands-svg-icons';
+import LogoS from './../../assets/images/logo-s.png';
+import LogoSubtitle from './../../assets/images/Sibt-e-Ali.png';
 const About = () => {
   const [letterClass, setLetterClass] = useState('text-animate');
   useEffect(() => {
@@ -19,55 +21,67 @@ const About = () => {
   }, []);
 
   return (
-    <div className="container about-page">
-      <div className="text-zone">
-        <h1>
-          <AnimtaedLetters
-            letterClass={letterClass}
-            strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
-            idx={15}
-          />
-        </h1>
-        <p>
-          I'm a MERN Stack developer and Freelancer looking forward to work with
-          diversed background IT company large or small but making an impact on
-          people's lives{' '}
-        </p>
-        <p>
-          I also love to work in startups due to their struggling envoirment to
-          survive and learn about things we have never learned before{' '}
-        </p>
-        <p>
-          In short, I'm a freelancer, gamer, singer, speaker, traveler and coder
-          by passion
-        </p>
-      </div>
-      <div className="stage-cube-cont">
-        <div className="cubespinner">
-          <div className="face1">
-            <FontAwesomeIcon icon={faNodeJs} color="#83CD29" />
-          </div>
-          <div className="face2">
-            <FontAwesomeIcon icon={faHtml5} color="#F06529" />
-          </div>
-          <div className="face3">
-            <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
-          </div>
-          <div className="face4">
-            <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
-          </div>
-          <div className="face5">
-            <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
-          </div>
-          <div className="face6">
-            <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
-          </div>
+    <>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimtaedLetters
+              letterClass={letterClass}
+              strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            I'm a MERN Stack developer and Freelancer looking forward to work
+            with diversed background IT company large or small but making an
+            impact on people's lives{' '}
+          </p>
+          <p>
+            I also love to work in startups due to their struggling envoirment
+            to survive and learn about things we have never learned before{' '}
+          </p>
+          <p>
+            In short, I'm a freelancer, gamer, singer, speaker, traveler and
+            coder by passion
+          </p>
         </div>
-        <div class="particle particle-1"></div>
-        <div class="particle particle-4"></div>
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faNodeJs} color="#83CD29" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+            </div>
+          </div>
+          <div class="particle particle-1"></div>
+          <div class="particle particle-4"></div>
+        </div>
       </div>
-
-    </div>
+      <div class="loader logo">
+        <img src={LogoS} alt="logo" />
+        <img
+          className="sub-logo"
+          style={{ width: '60px' }}
+          src={LogoSubtitle}
+          alt="logo subtitle"
+        />
+        <br />
+        <span>Sibteali Baqar Working</span>
+      </div>
+    </>
   );
 };
 
