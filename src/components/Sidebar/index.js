d170c197829d0dc47aa16ab0a01ed17e2307cac9
sibteali786 <sibteali786@gmail.com>
2022-07-08 21:29:58 +0500

    first commit

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,54 @@
+import './index.scss';
+
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import LogoS from './../../assets/images/logo-s.png';
+import LogoSubtitle from './../../assets/images/Sibt-e-Ali.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin, faMedium } from '@fortawesome/free-brands-svg-icons';
+const Sidebar = () => {
+  return (
+    <div className="nav-bar">
+      <Link className="logo" to="/">
+        <img src={LogoS} alt="logo" />
+        <img className="sub-logo" src={LogoSubtitle} alt="logo subtitle" />
+      </Link>
+      <nav>
+        <NavLink exact="true" activeclassname="active" to="/">
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          to="/about"
+          className="about-link"
+        >
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink exact="true" activeclassname="active" to="/contact " className="contact-link">
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+      </nav>
+      <ul>
+        <li>
+          <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/syed-sibteali-baqar-03167a17a/">
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a target="_blank" rel="noreferrer" href="https://github.com/sibteali786">
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a target="_blank" rel="noreferrer" href="https://medium.com/@sibteali786">
+            <FontAwesomeIcon icon={faMedium} color="#4d4d4e" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
