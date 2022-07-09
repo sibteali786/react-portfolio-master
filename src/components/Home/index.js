01cd4b10f600b946a207a955f37d0acf1e4800d6
sibteali786 <sibteali786@gmail.com>
2022-07-09 22:11:58 +0500

    made changes in animation homepage and about

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -7,7 +7,7 @@ import Logo from './Logo';
 import LogoSubtitle from './../../assets/images/Sibt-e-Ali.png';
 const Home = () => {
   const [letterClass,setLetterClass] = useState('text-animate');
-  const nameArray = ['i','b','t','-','e','-','A','l','i'];
+  const nameArray = ['i','b','t','-','e','-','A','l','i',','];
   const jobArray = ['w','e','b',' ','d','e','v','e','l','o','p','e','r','.'];
   const [loading,setLoading] = useState(false);
   useEffect(() => {
@@ -25,14 +25,15 @@ const Home = () => {
       <div className="text-zone">
         <h1>
           <span className={letterClass}>H</span>
-          <span className={`${letterClass} _2`}>i,</span>
+          <span className={`${letterClass} _11`}>i</span>
+          <span className={`${letterClass} _12`}>,</span>
           <br/>
-          <span className={`${letterClass} _3`}>I</span>
-          <span className={`${letterClass} _4`}>'m</span>
+          <span className={`${letterClass} _13`}>I</span>
+          <span className={`${letterClass} _14`}>'m</span>
         <img src={LogoS} alt='developer'/>
-        <AnimtaedLetters letterClass={letterClass} strArray={nameArray} idx={5} />
+        <AnimtaedLetters letterClass={letterClass} strArray={nameArray} idx={15} />
         <br/>
-        <AnimtaedLetters letterClass={letterClass} strArray={jobArray} idx={9} />
+        <AnimtaedLetters letterClass={letterClass} strArray={jobArray} idx={22} />
         </h1>
         <h2>Frontend Developer / ReactJs / MERN Stack</h2>
         <Link to="/contact" className='flat-button'>CONTACT ME</Link>
