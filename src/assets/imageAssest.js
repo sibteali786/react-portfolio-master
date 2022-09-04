64d90be53fce686b3a39333dc84aa57785e8e3c0
Syed_Sibteali_Baqar <sibteali786@gmail.com>
2022-09-05 01:39:53 +0500

    added projects

---
+++ src/assets/imageAssest.js
@@ -0,0 +1,28 @@
+const repo_images = [
+  {
+    name: 'GuleShaor',
+    url: 'https://repository-images.githubusercontent.com/516148481/47d1ec40-199f-4f79-8b87-f33b68f4775c',
+  },
+  {
+    name: 'expense-tracker',
+    url: 'https://repository-images.githubusercontent.com/387848637/d5c36d41-0a72-4a0f-a776-f7c039d6981f',
+  },
+  {
+    name: 'Trax',
+    url: 'https://repository-images.githubusercontent.com/519408165/37e9a6bc-35be-454b-be93-0f66b641c220',
+  },
+  {
+    name: 'shoe-store',
+    url: 'https://repository-images.githubusercontent.com/395619546/cafbebc1-5c7f-4ed6-9479-f1e5e28a15b8',
+  },
+  {
+    name: 'short-animation',
+    url: 'https://repository-images.githubusercontent.com/403302307/3d7a5af7-170b-460f-a4c9-7022ec8fac84',
+  },
+  {
+    name: 'Proshop',
+    url: 'https://repository-images.githubusercontent.com/509396242/a7cc6a87-7ab0-420e-9c8e-088d0ffe9c83',
+  },
+];
+
+export default repo_images;
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -3,12 +3,13 @@ import './index.scss';
 import LogoS from './../../assets/images/logo-s.png';
 import LogoSubtitle from './../../assets/images/Sibt-e-Ali.png';
 import { Octokit } from 'octokit';
+import repo_images from '../../assets/imageAssest';
 
 const Project = () => {
   const [repos, setRepos] = useState([]);
   const [loading, setLoading] = useState(true);
   const octokit = new Octokit({
-    auth: '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',
+    auth: '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',
   });
   useEffect(() => {
     if (repos.length !== 0) {
@@ -24,6 +25,7 @@ const Project = () => {
     if (repos.length === 0 && loading) {
       fetchRepos().catch(console.error);
     }
+    // eslint-disable-next-line
   }, [loading, repos]);
 
   const sliderHandler = (e) => {
@@ -73,165 +75,57 @@ const Project = () => {
       {!loading && repos.length !== 0 ? (
         <>
           <div id="slider__warpper">
-            <div
-              className="flex__container flex--pikachu flex--active"
-              data-slide="1"
-            >
-              <div className="flex__item flex__item--left">
-                <div className="flex__content">
-                  <p className="text--sub">Pokemon Gen I</p>
-                  <h1 className="text--big">Pikachu</h1>
-                  <p className="text--normal">
-                    Pikachu is an Electric-type Pokémon introduced in Generation
-                    I. Pikachu are small, chubby, and incredibly cute mouse-like
-                    Pokémon. They are almost completely covered by yellow fur.
-                  </p>
+            {repos.map((repo, index) => {
+              return (
+                <div
+                  className={`flex__container ${
+                    index === 0 ? 'flex--active' : ''
+                  }`}
+                  data-slide={index + 1}
+                  key={index}
+                >
+                  <div className="flex__item flex__item--left">
+                    <div className="flex__content">
+                      <h1 className="text--big">{repo.name.toUpperCase()}</h1>
+                      <p className="text--sub">{repo.full_name}</p>
+                      <p className="text--normal">{repo.description}</p>
+                      <a
+                        href={repo.html_url}
+                        className="actionButton"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        Link To Repo
+                      </a>
+                    </div>
+                    <p className="text__background">{repo.name}</p>
+                  </div>
+                  <div className="flex__item flex__item--right"></div>
+                  {repo_images.map((image, index) => (
+                    <img
+                      className="pokemon__img"
+                      src={
+                        repo.name.toUpperCase() === image.name.toUpperCase()
+                          ? `${image.url}`
+                          : ''
+                      }
+                      alt="pikachu"
+                    />
+                  ))}
                 </div>
-                <p className="text__background">Pikachu</p>
-              </div>
-              <div className="flex__item flex__item--right"></div>
-              <img
-                className="pokemon__img"
-                src="https://s4.postimg.org/fucnrdeq5/pikachu.png"
-                alt="pikachu"
-              />
+              );
+            })}
+            <div className="slider__navi">
+              {repos.map((item, index) => (
+                <button
+                  className={`slide-nav  ${index === 0 ? 'active' : ''}`}
+                  data-slide={index + 1}
+                  onClick={(e) => sliderHandler(e)}
+                >
+                  {item.name}
+                </button>
+              ))}
             </div>
-            <div
-              className="flex__container flex--piplup animate--start"
-              data-slide="2"
-            >
-              <div className="flex__item flex__item--left">
-                <div className="flex__content">
-                  <p className="text--sub">Pokemon Gen IV</p>
-                  <h1 className="text--big">Piplup</h1>
-                  <p className="text--normal">
-                    Piplup is the Water-type Starter Pokémon of the Sinnoh
-                    region. It was introduced in Generation IV. Piplup has a
-                    strong sense of self-esteem. It seldom accepts food that
-                    people give because of its pride.
-                  </p>
-                </div>
-                <p className="text__background">Piplup</p>
-              </div>
-              <div className="flex__item flex__item--right"></div>
-              <img
-                className="pokemon__img"
-                src="https://s4.postimg.org/sa9dl4825/pilup.png"
-                alt="pilup"
-              />
-            </div>
-            <div
-              className="flex__container flex--blaziken animate--start"
-              data-slide="3"
-            >
-              <div className="flex__item flex__item--left">
-                <div className="flex__content">
-                  <p className="text--sub">Pokemon Gen III</p>
-                  <h1 className="text--big">Blaziken</h1>
-                  <p className="text--normal">
-                    Blaziken is the Fire/Fighting-type Starter Pokémon of the
-                    Hoenn region, introduced in Generation III. Blaziken is a
-                    large, bipedal, humanoid bird-like Pokémon that resembles a
-                    rooster.
-                  </p>
-                </div>
-                <p className="text__background">Blaziken</p>
-              </div>
-              <div className="flex__item flex__item--right"></div>
-              <img
-                className="pokemon__img"
-                src="https://s4.postimg.org/6795hnlql/blaziken.png"
-                alt="blaziken"
-              />
-            </div>
-            <div
-              className="flex__container flex--dialga animate--start"
-              data-slide="4"
-            >
-              <div className="flex__item flex__item--left">
-                <div className="flex__content">
-                  <p className="text--sub">Pokemon Gen IV</p>
-                  <h1 className="text--big">Dialga</h1>
-                  <p className="text--normal">
-                    Dialga is a Steel/Dragon-type Legendary Pokémon. Dialga is a
-                    sauropod-like Pokémon. It is mainly blue with some gray,
-                    metallic portions, such as its chest plate, which has a
-                    diamond in the center. It also has various, light blue lines
-                    all over its body.
-                  </p>
-                </div>
-                <p className="text__background">Dialga</p>
-              </div>
-              <div className="flex__item flex__item--right"></div>
-              <img
-                className="pokemon__img"
-                src="https://s4.postimg.org/43yq9zlxp/dialga.png"
-                alt="dialpa"
-              />
-            </div>
-            <div
-              className="flex__container flex--zekrom animate--start"
-              data-slide="5"
-            >
-              <div className="flex__item flex__item--left">
-                <div className="flex__content">
-                  <p className="text--sub">Pokemon Gen V</p>
-                  <h1 className="text--big">Zekrom</h1>
-                  <p className="text--normal">
-                    Zekrom is a Dragon/Electric-type Legendary Pokémon. It is
-                    part of the Tao Trio, along with Reshiram and Kyurem. Zekrom
-                    is a large, black draconian Pokémon that seems to share its
-                    theme with its counterpart, Reshiram. It has piercing red
-                    eyes and dark gray to black skin that seems to be
-                    armor-like.
-                  </p>
-                </div>
-                <p className="text__background">Zekrom</p>
-              </div>
-              <div className="flex__item flex__item--right"></div>
-              <img
-                className="pokemon__img"
-                src="https://s4.postimg.org/malmhgn9p/zekrom.png"
-                alt="zekrom"
-              />
-            </div>
-          </div>
-          <div className="slider__navi">
-            <button
-              className="slide-nav active"
-              data-slide="1"
-              onClick={(e) => sliderHandler(e)}
-            >
-              pikachu
-            </button>
-            <button
-              className="slide-nav"
-              data-slide="2"
-              onClick={(e) => sliderHandler(e)}
-            >
-              piplup
-            </button>
-            <button
-              className="slide-nav"
-              data-slide="3"
-              onClick={(e) => sliderHandler(e)}
-            >
-              blaziken
-            </button>
-            <button
-              className="slide-nav"
-              data-slide="4"
-              onClick={(e) => sliderHandler(e)}
-            >
-              dialga
-            </button>
-            <button
-              className="slide-nav"
-              data-slide="5"
-              onClick={(e) => sliderHandler(e)}
-            >
-              zekrom
-            </button>
           </div>
         </>
       ) : (
